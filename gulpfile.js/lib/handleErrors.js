69d1af5b4d66a819058144a081ef6c0941a7d038
张少辉 <zhangshaohui@panda.tv>
2017-06-19 15:46:25 +0800

    add bin and eslint

--- gulpfile.js/lib/handleErrors.js
+++ gulpfile.js/lib/handleErrors.js
@@ -4,10 +4,5 @@ var notify = require("gulp-notify");
 
 module.exports = function(errorObject, callback) {
     notify.onError(errorObject.toString().split(': ').join(':\n')).apply(this, arguments);
-console.log(this);
-
-    // 防止gulp进程挂掉
-    if (typeof this.emit === 'function') {
-        this.emit('end');
-    }
+    process.exit(1);
 };
